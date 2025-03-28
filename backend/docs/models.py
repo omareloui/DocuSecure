b286e9e0b2c2d71debabca35911c0ac97ca45d14
Omar Eloui <contact@omareloui.com>
2025-03-28 03:17:05 +0000

    feat(doc): add the extra fields needed for the requerments endpoints

--- backend/docs/models.py
+++ backend/docs/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from .lib.parse_doc import FileParser, get_parser_from_mimetype
 
@@ -16,12 +17,37 @@ class BaseModel(models.Model):
 
 
 class Doc(BaseModel):
+    class DocStatus(models.TextChoices):
+        PENDING = "PENDING", _("Pending Parsing")
+        PARSED = "PARSED", _("Parsed")
+
     content = models.TextField()
     file = models.FileField(upload_to=UPLOAD_TO)
+    filename = models.CharField(max_length=64)
+    path = models.CharField(max_length=64)
+    url = models.CharField(max_length=64)
+    size = models.IntegerField()
     mimetype = models.CharField(max_length=64)
+
+    classification = models.CharField(null=True, max_length=64)
+    confidence = models.FloatField(default=0)
+
+    # TODO: for now it'll be a comma separated list
+    keywords = models.TextField(null=True)
+
+    status = models.CharField(
+        max_length=16,
+        choices=DocStatus,
+        default=DocStatus.PENDING,
+    )
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     _parser = None
+
+    def set_file_metadata(self):
+        self.path = self.file.path
+        self.url = self.file.url
+        self.save()
 
     def set_content_from_file(self):
         if not self._parser:
--- backend/docs/views.py
+++ backend/docs/views.py
@@ -32,33 +32,24 @@ def index(request):
 @login_required()
 @permission_required("docs.add_doc", raise_exception=True)
 def upload(request):
-    logger = getLogger("loggers")
-
     if request.method == "GET":
         return render(request, "docs/upload.html", {"form": UplaodDocumentForm()})
 
-    mimetype = request.FILES.get("file").content_type
-
-    doc = Doc(owner=request.user, mimetype=mimetype)
-
-    logger.info(
-        {"message": "Creating a new document", "doc": doc, "mimetype": mimetype}
-    )
+    logger = getLogger("loggers")
 
     form = UplaodDocumentForm(
         request.POST,
         request.FILES,
-        instance=doc,
     )
     if not form.is_valid():
+        logger.info({"message": "Invlid form"})
         return render(
             request,
             "docs/upload.html",
             {"form": form},
         )
 
-    form.save()
-    doc.set_content_from_file()
+    save_file(form.cleaned_data["file"], request.user)
     return redirect("/")
 
 
@@ -66,13 +57,12 @@ def upload(request):
 @login_required()
 @permission_required("docs.add_doc", raise_exception=True)
 def bulk_upload(request):
-    logger = getLogger("loggers")
-
     if request.method == "GET":
         return render(request, "docs/bulk-upload.html", {"form": BulkUpload()})
 
-    form = BulkUpload(request.POST, request.FILES)
+    logger = getLogger("loggers")
 
+    form = BulkUpload(request.POST, request.FILES)
     if not form.is_valid():
         logger.info({"message": "Error validating the form"})
         return render(request, "docs/bulk-upload.html", {"form": form})
@@ -82,9 +72,7 @@ def bulk_upload(request):
     logger.info({"files_count": len(files)})
 
     for f in files:
-        doc = Doc(file=f, owner=request.user, mimetype=f.content_type)
-        doc.save()
-        doc.set_content_from_file()
+        save_file(f, request.user)
 
     return redirect("/")
 
@@ -99,13 +87,24 @@ def delete(request, id):
         {"id": id, "action": "DELETE", "subject": "docs", "doc": doc, "file": doc.file}
     )
 
-    _path = doc.file.path
-
     try:
-        os.unlink(_path)
+        os.unlink(doc.path)
     except FileNotFoundError:
         logger.warning({"message": "Couldn't find file to delete", "path": _path})
         pass
 
     doc.delete()
     return redirect("docs")
+
+
+def save_file(file, user):
+    doc = Doc(
+        file=file,
+        filename=file.name,
+        size=file.size,
+        owner=user,
+        mimetype=file.content_type,
+    )
+    doc.save()
+    doc.set_file_metadata()
+    doc.set_content_from_file()
