e21e455697b94f3803ccb305654115f6e5f48579
Omar Eloui <contact@omareloui.com>
2025-03-31 21:58:39 +0000

    test(search): write tests for the search app

--- backend/search/tests.py
+++ backend/search/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import Client, TestCase
+from docs.models import Doc
+from elasticsearch_dsl import Q
+from search.documents import DocDocument
 
-# Create your tests here.
+
+class DocTestCase(TestCase):
+    def setUp(self):
+        self.c = Client()
+
+        user = User.objects.create_superuser(username="omareloui", password="123456")
+        self.c.force_login(user)
+
+    def test_creating_elastic_document_on_creating_document(self):
+        filename = "requirements.txt"
+        with open(filename, "rb") as fp:
+            self.c.post("/upload", {"file": fp})
+
+        _q = Q("term", filename=filename)
+        docs = DocDocument.search().query(_q).execute()
+        self.assertIsNotNone(docs)
+        self.assertGreater(len(docs), 0)
+
+    def test_on_deleting_the_document_is_deleted(self):
+        filename = "requirements.txt"
+        with open(filename, "rb") as fp:
+            self.c.post("/upload", {"file": fp})
+
+        _q = Q("term", filename=filename)
+        docs = DocDocument.search().query(_q).execute()
+        self.assertIsNotNone(docs)
+        self.assertEqual(len(docs), 1)
+
+        created_doc = Doc.objects.get(filename=filename)
+
+        self.assertIsNotNone(created_doc)
+        created_doc.delete()
+
+        docs_after_deletion = DocDocument.search().query(_q).execute()
+        self.assertEqual(len(docs_after_deletion), 0)
